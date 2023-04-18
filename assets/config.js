4701a7037570b7c052f9da9e5e5cf4070dc7c554
Tossawat Umpharote <t_umpharote@hotmail.com>
2023-04-18 11:54:24 +0700

    first commit

---
+++ assets/config.js
@@ -0,0 +1,23 @@
+let config = {
+  "Title": "Chutima Sutipayakul - Links",
+  "Name": "Chutima Sutipayakul (Ae)",
+  "Description": "Hello, My contact point is the below description.",
+  "links": [
+    {
+      "Title": "E-Contact Card",
+      "URL": "https://tossawat.in.th/contact_profiles/chutima@cititex.co.th.vcf",
+      "icon_classes": "fas fa-address-card"
+    },
+    {
+      "Title": "Email",
+      "URL": "mailto:chutima@cititex.co.th",
+      "icon_classes": "fas fa-envelope"
+    },
+    {
+      "Title": "Facebook",
+      "URL": "https://www.facebook.com/chutima.sutipayakul/",
+      "icon_classes": "fab fa-facebook"
+    },
+
+  ]
+}
